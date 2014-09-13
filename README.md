58744cc91916c8b8aca852d1c1fda7bef5913703
qqibrow <qqibrow@gmail.com>
2014-09-13 07:11:33 +0000

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+setup
+=====
+
+Set up following development tools:
+
+1. vim.
+2. screen.
+
+
---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp ./cpp.snip ~/.vim/bundle/neosnippet-snippets/neosnippets/cpp.snip 
+cp ./.screenrc ~/.screenrc
+cp ./.vimrc.local ~/.vimrc.local
