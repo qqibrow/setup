a782bfe85784af832ff42b1097b403af72d401f2
qqibrow <qqibrow@gmail.com>
2015-03-21 11:08:43 -0700

    add git related settings

--- setup.sh
+++ setup.sh
@@ -4,3 +4,11 @@ cp ./.screenrc ~/.screenrc
 cp ./.vimrc.local ~/.vimrc.local
 cp ./.bashrc ~/.bashrc
 cp ./git-completion.bash ~/git-completion.bash
+
+
+# setup git color (http://stackoverflow.com/questions/1156069/how-to-configure-mac-os-x-term-so-that-git-has-color)
+git config --global color.ui true
+
+# setup git user 
+git config --global user.name  "qqibrow"
+git config --global user.email  "qqibrow@gmail.com"
